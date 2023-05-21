0b4bc366fc44100328885c43d90c9b3031b5ed2f
tycho garen <garen@tychoish.com>
2023-05-20 20:28:15 -0400

    infra improvements

--- gadget/infra.go
+++ gadget/infra.go
@@ -14,11 +14,24 @@ import (
 	"time"
 
 	"github.com/tychoish/fun"
+	"github.com/tychoish/fun/adt"
 	"github.com/tychoish/fun/erc"
+	"github.com/tychoish/fun/itertool"
+	"github.com/tychoish/fun/set"
 	"github.com/tychoish/grip"
+	"github.com/tychoish/grip/level"
 	"github.com/tychoish/grip/message"
 	"github.com/tychoish/grip/send"
+	"github.com/tychoish/jasper"
+	"github.com/tychoish/jasper/util"
 )
+
+type IteratorWorker[T any] struct {
+	fun.Iterator[T]
+	adt.Atomic[fun.WorkerFunc]
+}
+
+func (iw IteratorWorker[T]) Close() error { return erc.Merge(iw.Iterator.Close(), iw.Get().Block()) }
 
 func WithTiming(name string, op func()) {
 	start := time.Now()
@@ -29,6 +42,85 @@ func WithTiming(name string, op func()) {
 	}()
 
 	op()
+}
+
+func prunePairs[K comparable, V any](iter fun.Iterator[fun.Pair[K, V]], limits ...K) fun.Iterator[fun.Pair[K, V]] {
+	limit := set.NewUnordered[K]()
+	fun.WorkerFunc(func(ctx context.Context) error {
+		set.PopulateSet(ctx, limit, itertool.Slice(limits))
+		return nil
+	}).MustWait().Block()
+
+	return fun.Filter(iter, func(pair fun.Pair[K, V]) bool { return limit.Check(pair.Key) })
+}
+
+type RipgrepArgs struct {
+	Types         []string
+	ExcludedTypes []string
+	Regexp        string
+	Path          string
+	IgnoreFile    string
+	Directories   bool
+	Unique        bool
+	Invert        bool
+	Zip           bool
+	WordRegexp    bool
+}
+
+func Ripgrep(ctx context.Context, jpm jasper.Manager, args RipgrepArgs) fun.Iterator[string] {
+	args.Path = util.TryExpandHomedir(args.Path)
+	sender := &bufsend{
+		buffer: &bytes.Buffer{},
+	}
+	sender.SetPriority(level.Info)
+	sender.SetName("ripgrep")
+	sender.SetErrorHandler(send.ErrorHandlerFromSender(grip.Sender()))
+
+	cmd := []string{"rg",
+		"--files-with-matches",
+		"--line-buffered",
+		"--color=never",
+		"--trim",
+	}
+	if args.Invert {
+		cmd = append(cmd, "--invert-match")
+	}
+	if args.IgnoreFile != "" {
+		cmd = append(cmd, "--ignore-file", args.IgnoreFile)
+	}
+	if args.Zip {
+		cmd = append(cmd, "--search-zip")
+	}
+	if args.WordRegexp {
+		cmd = append(cmd, "--word-regexp")
+	}
+	for _, t := range args.Types {
+		cmd = append(cmd, "--type", t)
+	}
+	for _, t := range args.ExcludedTypes {
+		cmd = append(cmd, "--type-not", t)
+	}
+	cmd = append(cmd, "--regexp", args.Regexp)
+
+	ec := &erc.Collector{}
+	ec.Add(jpm.CreateCommand(ctx).
+		Directory(args.Path).
+		Add(cmd).
+		SetOutputSender(level.Info, sender).
+		SetErrorSender(level.Error, grip.Sender()).
+		Run(ctx))
+
+	iter := LineIterator(strings.TrimSpace(sender.buffer.String()))
+
+	if args.Directories {
+		iter = fun.Transform(iter, func(in string) (string, error) { return filepath.Dir(in), nil })
+	}
+
+	if args.Unique {
+		iter = itertool.Uniq(iter)
+	}
+
+	return iter
 }
 
 // WalkDirIterator provides an alternate fun.Iterator-based interface
---
+++ gadget/infra_test.go
@@ -0,0 +1,41 @@
+package gadget
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/tychoish/fun"
+	"github.com/tychoish/grip"
+	"github.com/tychoish/grip/message"
+	"github.com/tychoish/jasper"
+	"github.com/tychoish/jasper/x/track"
+)
+
+func TestRipgrep(t *testing.T) {
+	start := time.Now()
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	jpm := jasper.NewManager(jasper.ManagerOptions{ID: t.Name(), Synchronized: true, MaxProcs: 64, Tracker: fun.Must(track.New(t.Name()))})
+	args := RipgrepArgs{
+		Types:       []string{"go"},
+		Regexp:      "go:generate",
+		Path:        "~/neon/cloud",
+		Directories: true,
+		Unique:      true,
+	}
+	iter := Ripgrep(ctx, jpm, args)
+
+	count := 0
+
+	for iter.Next(ctx) {
+		count++
+		grip.Info(iter.Value())
+	}
+	grip.Info(message.Fields{
+		"path":  args.Path,
+		"count": count,
+		"dur":   time.Since(start),
+	})
+}
